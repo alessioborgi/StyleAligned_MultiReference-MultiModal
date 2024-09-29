eb257c47b1416914357e42f72cbff360eb1deba5
alessioborgi <73711270+alessioborgi@users.noreply.github.com>
2024-09-29 07:45:02 +0200

    Adjusted also Multi-Style Linear to dynamic style arguments selection

--- src/Encode_Image.py
+++ src/Encode_Image.py
@@ -17,81 +17,6 @@ from src.Handler import Handler
 from diffusers import StableDiffusionXLPipeline
 from src.StyleAlignedArgs import StyleAlignedArgs
 
-
-def image_encoding(model: StableDiffusionXLPipeline, image: np.ndarray) -> T:
-
-    # 1) Set VAE to Float32: Ensure the VAE operates in float32 precision for encoding.
-    model.vae.to(dtype=torch.float32)
-
-    # 2) Convert Image to PyTorch Tensor: Convert the input image from a numpy array to a PyTorch tensor and normalize pixel values to [0, 1].
-    scaled_image = torch.from_numpy(image).float() / 255.
-
-    # 3) Normalize and Prepare Image: Scale pixel values to the range [-1, 1], rearrange dimensions, and add batch dimension.
-    permuted_image = (scaled_image * 2 - 1).permute(2, 0, 1).unsqueeze(0)
-
-    # 4) Encode Image Using VAE: Use the VAE to encode the image into the latent space.
-    latent_img = model.vae.encode(permuted_image.to(model.vae.device))['latent_dist'].mean * model.vae.config.scaling_factor
-
-    # 5) Reset VAE to Float16: Optionally reset the VAE to float16 precision.
-    model.vae.to(dtype=torch.float16)
-
-    # 6) Return Latent Representation: Return the encoded latent representation of the image.
-    return latent_img
-
-
-### LINEAR WEIGHTED AVERAGE #################################
-def images_encoding(model, images: list[np.ndarray], blending_weights: list[float]):
-    """
-    Encode a list of images using the VAE model and blend their latent representations
-    according to the given blending_weights.
-
-    Args:
-    - model: The StableDiffusionXLPipeline model.
-    - images: A list of numpy arrays, each representing an image.
-    - blending_weights: A list of floats representing the blending weights for each image.
-              The blending_weights should sum to 1.
-
-    Returns:
-    - blended_latent_img: The blended latent representation.
-    """
-
-    # Ensure the blending_weights sum to 1.
-    assert len(images) == len(blending_weights), "The number of images and blending_weights must match."
-    assert np.isclose(sum(blending_weights), 1.0), "blending_weights must sum to 1."
-
-    # Set VAE to Float32 for encoding.
-    model.vae.to(dtype=torch.float32)
-
-    # Initialize blended latent representation as None.
-    blended_latent_img = None
-
-    for img, weight in zip(images, blending_weights):
-
-        # Convert image to PyTorch tensor and normalize pixel values to [0, 1].
-        scaled_image = torch.from_numpy(img).float() / 255.
-
-        # Normalize and prepare image.
-        permuted_image = (scaled_image * 2 - 1).permute(2, 0, 1).unsqueeze(0)
-
-        # Encode image using VAE.
-        latent_img = model.vae.encode(permuted_image.to(model.vae.device))['latent_dist'].mean * model.vae.config.scaling_factor
-
-        # Blend the latent representation based on the weight.
-        if blended_latent_img is None:
-            blended_latent_img = latent_img * weight
-        else:
-            blended_latent_img += latent_img * weight
-
-    # Reset VAE to Float16 if necessary.
-    model.vae.to(dtype=torch.float16)
-
-    # Return the blended latent representation.
-    return blended_latent_img
-
-
-### WEIGHTED SLERP (SPHERICAL LINEAR INTERPOLATION) ###
-
-
 # 1) Normal Painting
 # These are some parameters you can Adjust to Control StyleAlignment to Reference Image.
 style_alignment_score_shift_normal = np.log(2)  # higher value induces higher fidelity, set 0 for no shift
@@ -123,6 +48,100 @@ famous_sa_args = StyleAlignedArgs(
     style_alignment_score_scale=style_alignment_score_scale_famous)
 
 
+
+############ SINGLE-STYLE REFERENCE #################################
+
+def image_encoding(model: StableDiffusionXLPipeline, image: np.ndarray) -> T:
+
+    # 1) Set VAE to Float32: Ensure the VAE operates in float32 precision for encoding.
+    model.vae.to(dtype=torch.float32)
+
+    # 2) Convert Image to PyTorch Tensor: Convert the input image from a numpy array to a PyTorch tensor and normalize pixel values to [0, 1].
+    scaled_image = torch.from_numpy(image).float() / 255.
+
+    # 3) Normalize and Prepare Image: Scale pixel values to the range [-1, 1], rearrange dimensions, and add batch dimension.
+    permuted_image = (scaled_image * 2 - 1).permute(2, 0, 1).unsqueeze(0)
+
+    # 4) Encode Image Using VAE: Use the VAE to encode the image into the latent space.
+    latent_img = model.vae.encode(permuted_image.to(model.vae.device))['latent_dist'].mean * model.vae.config.scaling_factor
+
+    # 5) Reset VAE to Float16: Optionally reset the VAE to float16 precision.
+    model.vae.to(dtype=torch.float16)
+
+    # 6) Return Latent Representation: Return the encoded latent representation of the image.
+    return latent_img
+
+
+############ MULTI-STYLE REFERENCE: LINEAR WEIGHTED AVERAGE #################################
+def images_encoding(model, images: list[np.ndarray], blending_weights: list[float], normal_famous_scaling: list[str], handler: Handler):
+    """
+    Encode a list of images using the VAE model and blend their latent representations
+    according to the given blending_weights.
+
+    Args:
+    - model: The StableDiffusionXLPipeline model.
+    - images: A list of numpy arrays, each representing an image.
+    - blending_weights: A list of floats representing the blending weights for each image.
+              The blending_weights should sum to 1.
+
+    Returns:
+    - blended_latent_img: The blended latent representation.
+    """
+
+    # Ensure the blending_weights sum to 1.
+    assert len(images) == len(blending_weights), "The number of images and blending_weights must match."
+    assert np.isclose(sum(blending_weights), 1.0), "blending_weights must sum to 1."
+    assert len(normal_famous_scaling) == len(images), "The number of scaling classifications must match the number of images."
+
+    # Set VAE to Float32 for encoding.
+    model.vae.to(dtype=torch.float32)
+
+    # Initialize blended latent representation as None.
+    blended_latent_img = None
+
+    for img, weight, scaling_type in zip(images, blending_weights, normal_famous_scaling):
+        
+        # Set VAE to Float32 for encoding.
+        model.vae.to(dtype=torch.float32)
+        # Check if the weight is greater than 0.
+        if weight > 0.0:
+
+            # Apply the style arguments dynamically
+            if scaling_type == "n":
+                handler.register(normal_sa_args)
+            elif scaling_type == "f":
+                handler.register(famous_sa_args)
+            else:
+                raise ValueError(f"Invalid scaling type: {scaling_type}")
+
+            # Convert image to PyTorch tensor and normalize pixel values to [0, 1].
+            scaled_image = torch.from_numpy(img).float() / 255.
+
+            # Normalize and prepare image.
+            permuted_image = (scaled_image * 2 - 1).permute(2, 0, 1).unsqueeze(0)
+
+            # Encode image using VAE.
+            latent_img = model.vae.encode(permuted_image.to(model.vae.device))['latent_dist'].mean * model.vae.config.scaling_factor
+
+            # Blend the latent representation based on the weight.
+            if blended_latent_img is None:
+                blended_latent_img = latent_img * weight
+            else:
+                blended_latent_img += latent_img * weight
+
+    # Reset VAE to Float16 if necessary.
+    model.vae.to(dtype=torch.float16)
+
+    # Return the blended latent representation.
+    return blended_latent_img
+
+
+
+
+
+
+
+############ MULTI-STYLE REFERENCE: WEIGHTED SLERP (SPHERICAL LINEAR INTERPOLATION) ###
 
 def weighted_slerp(weight, v0, v1):
     """Spherical linear interpolation with a weight factor."""
@@ -156,9 +175,6 @@ def images_encoding_slerp(model, images: list[np.ndarray], blending_weights: lis
     assert np.isclose(sum(blending_weights), 1.0), "blending_weights must sum to 1."
     assert len(normal_famous_scaling) == len(images), "The number of scaling classifications must match the number of images."
 
-    # Set VAE to Float32 for encoding.
-    # model.vae.to(dtype=torch.float32)
-
     # Initialize variables to store valid latents and corresponding weights
     valid_latents = []
     valid_weights = []
@@ -184,7 +200,6 @@ def images_encoding_slerp(model, images: list[np.ndarray], blending_weights: lis
             permuted_image = (scaled_image * 2 - 1).permute(2, 0, 1).unsqueeze(0)
 
             # Encode image using VAE.
-            latent_img = model.vae.encode(permuted_image.to(model.vae.device))['latent_dist'].mean * model.vae.config.scaling_factor
             latent_img = model.vae.encode(permuted_image.to(model.vae.device))['latent_dist'].mean * model.vae.config.scaling_factor
 
             # Store valid latent and weight
